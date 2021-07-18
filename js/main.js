13cddecfe676a6a9bdebbd9c120b5da688d73069
PDH <54939319+wow1186@users.noreply.github.com>
2021-07-18 11:01:00 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-/**
- * Created by Jerome on 03-03-16.
- */
-//noinspection JSCheckFunctionSignatures,JSCheckFunctionSignatures,JSCheckFunctionSignatures
 let game = new Phaser.Game(24*32, 17*32, Phaser.AUTO, document.getElementById('game'));
 game.state.add('Game',Game);
 game.state.start('Game');
